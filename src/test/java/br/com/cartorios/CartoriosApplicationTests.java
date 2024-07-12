618907e1e0b34be92ddbe51bf2dcb16f4712f508
Anderson Brito dos Santos <britoanderso@gmail.com>
2024-07-12 20:11:08 -0300

    commit inicial

---
+++ src/test/java/br/com/cartorios/CartoriosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cartorios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartoriosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
